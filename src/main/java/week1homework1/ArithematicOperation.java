2bb51c856b2999575b25e533fca7d76472c9f71e
USER <USER@PREETI>
2018-08-04 17:53:45 +0530

    arithmatic opreation code is shared pom.xml is shared

---
+++ src/main/java/week1homework1/ArithematicOperation.java
@@ -0,0 +1,34 @@
+package week1homework1;
+
+public class ArithematicOperation {
+	int numberOne=22 ;
+	int numberTwo=44;
+	int sub;int mul;int div;int mul2;int sum;
+public void addition() {
+	
+	System.out.println(sum=numberOne + numberTwo);
+	}
+public int substraction() {
+		//System.out.println("Enter two Number");
+		System.out.println(sub=numberOne - numberTwo);
+		return sub;
+		}
+public int multiplication() {
+		
+		//System.out.println("Enter two Number");
+		System.out.println(mul=numberOne * numberTwo);
+		return mul;
+		}
+public void multiplicationNumberByTwo() {
+	
+	//System.out.println("Enter numberOne=");
+	System.out.println(mul2=numberOne*2);
+        }
+public int DevideNumberTwoby(int divident) {
+	
+	//System.out.println("Enter numberTwo=");
+	System.out.println(div=numberTwo / divident);
+	return div;
+        }
+
+}
